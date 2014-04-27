77a95f7b33236c00144a2e8ec768b453fb396d60
Damien Villeneuve <BlinkSun@users.noreply.github.com>
2014-04-27 01:21:15 -0400

    Update EnderChest.php
    - take off Owner properties
    - 2 or more players cant be in the same enderchest at the same time

--- EnderChest.php
+++ EnderChest.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=EnderChest
 description=I hope you know what's an EnderChest :P
-version=1.0
+version=1.2
 author=BlinkSun
 class=EnderChest
 apiversion=11,12
@@ -13,20 +13,40 @@ apiversion=11,12
 class EnderChest implements Plugin
 {
 	private $api;
+	private $openEnderchest;
 
 	public function __construct(ServerAPI $api, $server = false)
 	{
-		$this->api = $api;	
+		$this->api = $api;
+		$this->openEnderchest = array();
 	}
-
+	
 	public function init()
 	{
+		DataPacketSendEvent::register(array($this, "dataPacketHandler"), EventPriority::HIGHEST);
 		AchievementAPI::addAchievement("enderChest","Keep my stuffs anywhere !");
 		$this->api->addHandler("player.block.touch", array($this, "eventHandle"), 50);
 		$this->api->addHandler("player.block.break", array($this, "eventHandle"), 50);
 		$this->api->addHandler("player.block.place", array($this, "eventHandle"), 50);
 		$this->api->addHandler("player.container.slot", array($this, "eventHandle"), 50);
 		$this->enderchests = new Config($this->api->plugin->configPath($this)."enderchests.yml", CONFIG_YAML);
+	}
+	
+	public function dataPacketHandler(DataPacketSendEvent $event){
+		$packet = $event->getPacket();
+
+		if($packet instanceof ContainerOpenPacket){
+			//console("openEnderchest(". $packet->x ."". $packet->y ."". $packet->z . ") = " . $packet->windowid);
+			$this->openEnderchest[$packet->x ."".$packet->y ."".$packet->z] = $packet->windowid;
+		}
+
+		if($packet instanceof ContainerClosePacket){
+			if(array_search($packet->windowid,$this->openEnderchest) !== false) {
+				//console("openEnderchest(" . array_search($packet->windowid,$this->openEnderchest) . ") = ". $packet->windowid);
+				unset($this->openEnderchest[array_search($packet->windowid,$this->openEnderchest)]);
+				$this->openEnderchest = array_values($this->openEnderchest);
+			}
+		}
 	}
 	
 	public function eventHandle($data, $event) {
@@ -40,36 +60,20 @@ class EnderChest implements Plugin
 				$tile = $this->api->tile->get(new Position($target->x, $target->y, $target->z, $target->level));
 				
 				if(($target->getID() == 54) and ($target->level->getBlock(new Vector3($target->x, $target->y - 1, $target->z))->getID() == 87)) {
-					if($this->enderchests->exists($target->x . "," . $target->y . "," . $target->z)) {
-						if($this->enderchests->get($target->x . "," . $target->y . "," . $target->z) == $player->username) {
-							
-							$slots = $this->enderchests->get($player->username);
-							
-							foreach ($slots as $key => $slot) {
-								$item = $this->api->block->getItem($slot[0], $slot[2], $slot[1]);
-								$tile->setSlot($key,$item);
-							}
-							break;
-							
-						} else {
-							$player->sendChat("[EnderChest] Sry, it's not your EnderChest!");
-							return false;
-						}
-						break;
-					
-					} else {
 
-						$player->sendChat("[EnderChest] This EnderChest is ownerless!");
-						
+					if(!isset($this->openEnderchest[$target->x . "" . $target->y . "" . $target->z])) {
+					
+						$slots = $this->enderchests->get($player->username);
+
 						if($this->enderchests->exists($player->username)) {
 							$slots = $this->enderchests->get($player->username);
-							$player->sendChat("[EnderChest] Now it's your EnderChest!");
 						} else {
 							$slots = array();
 							for($i = 0; $i < 27; $i++){
 								$slots[] = array(AIR,0,0);
 							}
-							$player->sendChat("[EnderChest] You got your first EnderChest!");
+							$this->enderchests->set($player->username,$slots);
+							$this->enderchests->save();
 							AchievementAPI::grantAchievement($player, "enderChest");
 						}
 						
@@ -77,14 +81,13 @@ class EnderChest implements Plugin
 							$item = $this->api->block->getItem($slot[0], $slot[2], $slot[1]);
 							$tile->setSlot($key,$item);
 						}
-						
-						$this->enderchests->set($target->x . "," . $target->y . "," . $target->z,$player->username);
-						$this->enderchests->set($player->username,$slots);
-						$this->enderchests->save();
+						break;
+					} else {
+						$player->sendChat("[ERROR] This Enderchest is already in use!");
+						return false;
 						break;
 					}
-					break;
-				}	
+				}
 				break;
 				
 			case "player.block.break":
@@ -94,29 +97,17 @@ class EnderChest implements Plugin
 				$tile = $this->api->tile->get(new Position($target->x, $target->y, $target->z, $target->level));
 				
 				if(($target->getID() == 54) and ($target->level->getBlock(new Vector3($target->x, $target->y - 1, $target->z))->getID() == 87)) {
-					if($this->enderchests->exists($target->x . "," . $target->y . "," . $target->z)) {
-						if($this->enderchests->get($target->x . "," . $target->y . "," . $target->z) == $player->username) {
-							
-							$emptyslots = array();
-							
-							for($i = 0; $i < 27; $i++){
-								$emptyslots[] = array(AIR,0,0);
-							}
-							foreach ($emptyslots as $key => $slot) {
-								$item = $this->api->block->getItem($slot[0], $slot[2], $slot[1]);
-								$tile->setSlot($key,$item);
-							}
-							
-							$this->enderchests->remove($target->x . "," . $target->y . "," . $target->z);
-							$this->enderchests->save();
-							$player->sendChat("[EnderChest] You have broken an EnderChest!");
-							break;
-							
-						} else {
-							$player->sendChat("[EnderChest] Sry, it's not your EnderChest!");
-							return false;
-						}
-						break;
+
+					if(isset($this->openEnderchest[$target->x . "" . $target->y . "" . $target->z])) return false;
+				
+					$emptyslots = array();
+					
+					for($i = 0; $i < 27; $i++){
+						$emptyslots[] = array(AIR,0,0);
+					}
+					foreach ($emptyslots as $key => $slot) {
+						$item = $this->api->block->getItem($slot[0], $slot[2], $slot[1]);
+						$tile->setSlot($key,$item);
 					}
 					break;
 				}
@@ -130,21 +121,14 @@ class EnderChest implements Plugin
 				
 				if($item->getID() == 54) {
 					if(($this->getSideChest($block) == false) and ($block->level->getBlock(new Vector3($block->x, $block->y - 1, $block->z))->getID() == 87)){
-
-						$this->enderchests->set($block->x . "," . $block->y . "," . $block->z,$player->username);
-						$this->enderchests->save();
-									
 						if(!$this->enderchests->exists($player->username)) {
 						
 							$emptyslots = array();	
 							for($i = 0; $i < 27; $i++){
 								$emptyslots[] = array(AIR,0,0);
 							}
-							
 							$this->enderchests->set($player->username,$emptyslots);
 							$this->enderchests->save();
-							
-							$player->sendChat("[EnderChest] You got your first EnderChest!");
 							AchievementAPI::grantAchievement($player, "enderChest");
 							break;
 						}
@@ -153,7 +137,7 @@ class EnderChest implements Plugin
 					} elseif($this->getSideChest($block) !== false) {
 					
 						if(($this->getSideChest($block)->level->getBlock(new Vector3($this->getSideChest($block)->x, $this->getSideChest($block)->y - 1, $this->getSideChest($block)->z))->getID() == 87) or ($block->level->getBlock(new Vector3($block->x, $block->y - 1, $block->z))->getID() == 87)) {
-							$player->sendChat("[EnderChest] You can't make big Enderchest!");
+							$player->sendChat("[ERROR] You can't build large Enderchest!");
 							return false;
 						}
 						break;
@@ -170,33 +154,30 @@ class EnderChest implements Plugin
 				$item = $data["itemdata"];
 				$player = $data["player"];
 
-				if($tile->class == TILE_CHEST and $this->enderchests->exists($tile->x . "," . $tile->y . "," . $tile->z)){
-					if($this->enderchests->get($tile->x . "," . $tile->y . "," . $tile->z) == $player->username) {
-						if($item->getID() !== AIR and $slot->getID() == $item->getID()){
-							if($slot->count < $item->count){
-								if($player->removeItem($item->getID(), $item->getMetadata(), $item->count - $slot->count, false) === false){
-									return false;
-								}
-							}elseif($slot->count > $item->count){
-								$player->addItem($item->getID(), $item->getMetadata(), $slot->count - $item->count, false);
-							}
-						}else{
-							if($player->removeItem($item->getID(), $item->getMetadata(), $item->count, false) === false){
+				if($tile->class == TILE_CHEST and $tile->level->getBlock(new Vector3($tile->x, $tile->y - 1, $tile->z))->getID() == 87){
+					if($item->getID() !== AIR and $slot->getID() == $item->getID()){
+						if($slot->count < $item->count){
+							if($player->removeItem($item->getID(), $item->getMetadata(), $item->count - $slot->count, false) === false){
 								return false;
 							}
-							$player->addItem($slot->getID(), $slot->getMetadata(), $slot->count, false);
+						}elseif($slot->count > $item->count){
+							$player->addItem($item->getID(), $item->getMetadata(), $slot->count - $item->count, false);
 						}
-						$tile->setSlot($pslot, $item);
-						
-						$slots = array();	
-						for($i = 0; $i < 27; $i++){
-							$slots[] = array($tile->getSlot($i)->getID(),$tile->getSlot($i)->count,$tile->getSlot($i)->getMetadata());
+					}else{
+						if($player->removeItem($item->getID(), $item->getMetadata(), $item->count, false) === false){
+							return false;
 						}
-
-						$this->enderchests->set($player->username,$slots);
-						$this->enderchests->save();
-						break;
+						$player->addItem($slot->getID(), $slot->getMetadata(), $slot->count, false);
 					}
+					$tile->setSlot($pslot, $item);
+					
+					$slots = array();	
+					for($i = 0; $i < 27; $i++){
+						$slots[] = array($tile->getSlot($i)->getID(),$tile->getSlot($i)->count,$tile->getSlot($i)->getMetadata());
+					}
+
+					$this->enderchests->set($player->username,$slots);
+					$this->enderchests->save();
 					break;
 				}
 				break;
